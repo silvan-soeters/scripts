fc20c836354292300e87749b061d8ebf15d9a43b
Silvan Soeters <silvansoeters@gmail.com>
2024-03-04 22:45:32 +0100

    Added reference to originating gallery

--- image-gallery.js
+++ image-gallery.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const createModal = () => {
+    const createModal = (galleryId) => {
         if (!document.getElementById('galleryModal')) {
             const modalHTML = `
-                <div id="galleryModal" class="gallery_modal" onclick="event.target.id === 'galleryModal' && (document.getElementById('galleryModal').style.display = 'none')">
+                <div id="galleryModal" class="gallery_modal ${galleryId}" onclick="event.target.id === 'galleryModal' && (document.getElementById('galleryModal').style.display = 'none')">
                     <button class="gallery_modal-close icon-embed-xsmall" id="galleryCloseModal"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--tabler" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 6L6 18M6 6l12 12"></path></svg></button>
                     <img id="galleryModalImage" class="gallery_modal-image" src="" onclick="event.stopPropagation()">
                     <button class="gallery_modal-previous icon-embed-xsmall" id="galleryPrevImage"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--tabler" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m12 8l-4 4l4 4m4-4H8"></path><path d="M20 6.667v10.666A2.667 2.667 0 0 1 17.333 20H6.667A2.667 2.667 0 0 1 4 17.333V6.667A2.667 2.667 0 0 1 6.667 4h10.666A2.667 2.667 0 0 1 20 6.667z"></path></g></svg></button>
@@ -32,22 +32,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const navigateThroughImages = (images, currentIndex, direction) => {
         let newIndex = currentIndex + direction;
-        if (newIndex < 0) {
-            newIndex = images.length - 1;
-        } else if (newIndex >= images.length) {
-            newIndex = 0;
-        }
+        newIndex = newIndex < 0 ? images.length - 1 : newIndex >= images.length ? 0 : newIndex;
         document.getElementById('galleryModalImage').src = images[newIndex].src;
-        // Update the current index for correct navigation
         currentImageIndex = newIndex;
     };
 
-    let currentImageIndex = 0; // Track the current image index for navigation
+    let currentImageIndex = 0;
 
     const initializeImageModal = (galleryId) => {
         const gallery = document.getElementById(galleryId);
         const images = gallery.querySelectorAll('.gallery_frame-image');
-        createModal();
+        createModal(galleryId);
 
         images.forEach((image, index) => {
             image.addEventListener('click', () => {
@@ -55,7 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 const modalImage = document.getElementById('galleryModalImage');
                 modalImage.src = image.src;
                 modal.style.display = 'flex';
-                currentImageIndex = index; // Set the current image index
+                modal.className = `gallery_modal ${galleryId}`;
+                currentImageIndex = index;
 
                 document.getElementById('galleryPrevImage').onclick = () => navigateThroughImages(images, currentImageIndex, -1);
                 document.getElementById('galleryNextImage').onclick = () => navigateThroughImages(images, currentImageIndex, 1);
@@ -63,7 +59,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    // Initialize the modal functionality for each gallery
     initializeImageModal('photography-gallery');
     initializeImageModal('illustration-gallery');
 });
