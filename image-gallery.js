47de39f001ee37cdb8f3940b801b61ca7f6d43c9
Silvan Soeters <silvansoeters@gmail.com>
2024-03-05 01:42:15 +0100

    Update image-gallery.js

--- image-gallery.js
+++ image-gallery.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!document.getElementById('galleryModal')) {
             const modalHTML = `
                 <div id="galleryModal" class="gallery_modal ${galleryId}" onclick="event.target.id === 'galleryModal' && (document.getElementById('galleryModal').style.display = 'none')">
-                    <button class="gallery_modal-close icon-embed-xsmall" id="galleryCloseModal">...</button>
+                    <button class="gallery_modal-close icon-embed-xsmall" id="galleryCloseModal"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--tabler" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 6L6 18M6 6l12 12"></path></svg></button>
                     <div class="gallery_modal-image-container">
                         <img id="galleryModalImage" class="gallery_modal-image" src="" onclick="event.stopPropagation()">
                     </div>
-                    <button class="gallery_modal-previous icon-embed-xsmall" id="galleryPrevImage">...</button>
-                    <button class="gallery_modal-next icon-embed-xsmall" id="galleryNextImage">...</button>
+                    <button class="gallery_modal-previous icon-embed-xsmall" id="galleryPrevImage"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--tabler" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m12 8l-4 4l4 4m4-4H8"></path><path d="M20 6.667v10.666A2.667 2.667 0 0 1 17.333 20H6.667A2.667 2.667 0 0 1 4 17.333V6.667A2.667 2.667 0 0 1 6.667 4h10.666A2.667 2.667 0 0 1 20 6.667z"></path></g></svg></button>
+                    <button class="gallery_modal-next icon-embed-xsmall" id="galleryNextImage"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--tabler" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m12 16l4-4l-4-4m-4 4h8"></path><path d="M4 17.333V6.667A2.667 2.667 0 0 1 6.667 4h10.666A2.667 2.667 0 0 1 20 6.667v10.666A2.667 2.667 0 0 1 17.333 20H6.667A2.667 2.667 0 0 1 4 17.333z"></path></g></svg></button>
                 </div>
             `;
             document.body.insertAdjacentHTML('beforeend', modalHTML);
