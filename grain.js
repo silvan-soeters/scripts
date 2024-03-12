c871c7b1b41f4108690d34236a1c5778f4ce30cd
Silvan Soeters <silvansoeters@gmail.com>
2024-03-12 02:03:25 +0100

    Create grain.js

---
+++ grain.js
@@ -0,0 +1,61 @@
+// Get the icon element
+const toggleIcon = document.querySelector('.sidebar_menu-grain-toggle');
+
+// Initialize a boolean variable to keep track of the state
+let isGrainedVisible = false;
+
+// Define the options for the grained function
+const options = {
+  animate: true,
+  patternWidth: 500,
+  patternHeight: 500,
+  grainOpacity: 0.05,
+  grainDensity: 10,
+  grainWidth: 4,
+  grainHeight: 4
+};
+
+// Function to apply the grained effect to elements
+const applyGrainedEffect = () => {
+  const grainBody = document.querySelector('#grain-body');
+  const grainSidebar = document.querySelector('#grain-sidebar');
+  const grainProfile = document.querySelector('#grain-profile');
+
+  if (grainBody) {
+    grained('#grain-body', options);
+    grainBody.style.position = 'fixed';
+    grainBody.style.display = 'none'; // Hide the grained body initially
+  }
+  if (grainSidebar) {
+    grained('#grain-sidebar', options);
+  }
+  if (grainProfile) {
+    grained('#grain-profile', options);
+  }
+};
+
+// Function to show/hide the grained body element
+const toggleGrainedVisibility = () => {
+  const grainBody = document.querySelector('#grain-body');
+
+  isGrainedVisible = !isGrainedVisible; // Toggle the state
+
+  // Show/hide the grained body element based on the state
+  grainBody.style.display = isGrainedVisible ? 'block' : 'none';
+};
+
+// Apply the grained effect to elements on page load
+applyGrainedEffect();
+
+// Add an event listener to the icon element
+toggleIcon.addEventListener('click', toggleGrainedVisibility);
+</script>
+<script>
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        const formModal = document.querySelector('.multi-step-form_component');
+        if (formModal && formModal.style.display === 'flex') {
+            formModal.style.display = 'none';
+        }
+    }
+});
