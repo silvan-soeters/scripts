ad1bbe02ee48872542a8824fa35688b118793773
Silvan Soeters <silvansoeters@gmail.com>
2024-03-17 06:11:31 +0100

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,23 +1,16 @@
-// Tabs Functionality
-const tabsLinks = document.querySelectorAll(".tab_tab-link");
-const tabsPanes = document.querySelectorAll(".tab_tab-pane");
-
-// Add click event listener to each tab link
 tabsLinks.forEach((link, index) => {
   link.addEventListener("click", () => {
-    // Remove 'is-active' class from all links and panes
-    tabsLinks.forEach((tabLink) => tabLink.classList.remove("is-active"));
-    tabsPanes.forEach((tabPane) => tabPane.classList.remove("is-active"));
+    if (link.classList.contains("is-active")) return;
 
-    // Add 'is-active' class to the clicked link
-    link.classList.add("is-active");
+    const activePane = document.querySelector(".tab_tab-pane.is-active");
+    activePane.classList.remove("is-active");
 
-    // Delay adding 'is-active' class to the related pane for animation
     setTimeout(() => {
+      tabsLinks.forEach((tabLink) => tabLink.classList.remove("is-active"));
+      tabsPanes.forEach((tabPane) => tabPane.classList.remove("is-active"));
+
+      link.classList.add("is-active");
       tabsPanes[index].classList.add("is-active");
-    }, 150);
+    }, 500);
   });
 });
-
-// Initialize the first tab as active
-tabsLinks[0].click();
