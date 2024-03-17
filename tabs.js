0f8c716837b8a45be383d69921f1aea7685e4c35
Silvan Soeters <silvansoeters@gmail.com>
2024-03-17 06:28:23 +0100

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -6,14 +6,15 @@ tabsLinks.forEach((link, index) => {
   link.addEventListener("click", () => {
     if (link.classList.contains("is-active")) return;
 
+    const activeLink = document.querySelector(".tab_tab-link.is-active");
     const activePane = document.querySelector(".tab_tab-pane.is-active");
+
+    activeLink.classList.remove("is-active");
     activePane.classList.remove("is-active");
 
-    setTimeout(() => {
-      tabsLinks.forEach((tabLink) => tabLink.classList.remove("is-active"));
-      tabsPanes.forEach((tabPane) => tabPane.classList.remove("is-active"));
+    link.classList.add("is-active");
 
-      link.classList.add("is-active");
+    setTimeout(() => {
       tabsPanes[index].classList.add("is-active");
     }, 500);
   });
