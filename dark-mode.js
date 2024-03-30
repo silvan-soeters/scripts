db8d30f7c14305d3b3a3dde49dd69ff75e6724f0
Silvan Soeters <silvansoeters@gmail.com>
2024-03-30 23:07:54 +0100

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,24 +1,28 @@
-const themeToggle = document.querySelector('.toggle-checkbox');
-const moonIcon = document.querySelector('#dark-mode-moon');
-const sunIcon = document.querySelector('#dark-mode-sun');
+const themeToggles = document.querySelectorAll('.topbar_dark-mode-toggle');
 const themeColorMeta = document.getElementById('theme-color-meta');
 
-function updateTheme(isDarkMode) {
-  document.body.classList.toggle('theme-dark', isDarkMode);
-  themeColorMeta.setAttribute('content', isDarkMode ? '#111111' : '#c9c9c9');
-  moonIcon.style.opacity = isDarkMode ? 1 : 0;
-  sunIcon.style.opacity = isDarkMode ? 0 : 1;
-}
+themeToggles.forEach(function(toggleButton) {
+  const themeToggle = toggleButton.querySelector('.toggle-checkbox');
+  const moonIcon = toggleButton.querySelector('#dark-mode-moon');
+  const sunIcon = toggleButton.querySelector('#dark-mode-sun');
 
-function toggleTheme() {
-  themeToggle.checked = !themeToggle.checked;
-  updateTheme(themeToggle.checked);
-}
+  const updateTheme = function(isDarkMode) {
+    document.body.classList.toggle('theme-dark', isDarkMode);
+    themeColorMeta.setAttribute('content', isDarkMode ? '#111111' : '#c9c9c9');
+    moonIcon.style.opacity = isDarkMode ? 1 : 0;
+    sunIcon.style.opacity = isDarkMode ? 0 : 1;
+  };
 
-document.querySelector('.topbar_dark-mode-toggle').addEventListener('click', toggleTheme);
-document.querySelector('.topbar_dark-mode-toggle').addEventListener('keydown', function(event) {
-  if (event.code === 'Space' || event.code === 'Enter') {
-    event.preventDefault();
-    toggleTheme();
-  }
+  toggleButton.addEventListener('click', function() {
+    themeToggle.checked = !themeToggle.checked;
+    updateTheme(themeToggle.checked);
+  });
+
+  toggleButton.addEventListener('keydown', function(event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+      event.preventDefault();
+      themeToggle.checked = !themeToggle.checked;
+      updateTheme(themeToggle.checked);
+    }
+  });
 });
