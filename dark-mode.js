1933b08bb8c4e245654910b346b323199719c1e6
Silvan Soeters <silvansoeters@gmail.com>
2024-03-30 23:01:43 +0100

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,34 +1,24 @@
-const themeToggles = document.querySelectorAll('.topbar\_dark-mode-toggle');
+const themeToggle = document.querySelector('.toggle-checkbox');
+const moonIcon = document.querySelector('#dark-mode-moon');
+const sunIcon = document.querySelector('#dark-mode-sun');
 const themeColorMeta = document.getElementById('theme-color-meta');
 
-themeToggles.forEach(function(toggleButton) {
-  const themeToggle = toggleButton.querySelector('.toggle-checkbox');
-  const moonIcon = toggleButton.querySelector('#dark-mode-moon');
-  const sunIcon = toggleButton.querySelector('#dark-mode-sun');
+function updateTheme(isDarkMode) {
+  document.body.classList.toggle('theme-dark', isDarkMode);
+  themeColorMeta.setAttribute('content', isDarkMode ? '#111111' : '#c9c9c9');
+  moonIcon.style.opacity = isDarkMode ? 1 : 0;
+  sunIcon.style.opacity = isDarkMode ? 0 : 1;
+}
 
-  toggleButton.addEventListener('click', function() {
-    themeToggle.checked = !themeToggle.checked;
-    updateTheme(themeToggle.checked);
-  });
+function toggleTheme() {
+  themeToggle.checked = !themeToggle.checked;
+  updateTheme(themeToggle.checked);
+}
 
-  toggleButton.addEventListener('keydown', function(event) {
-    if (event.code === 'Space' || event.code === 'Enter') {
-      event.preventDefault();
-      themeToggle.checked = !themeToggle.checked;
-      updateTheme(themeToggle.checked);
-    }
-  });
-
-  const updateTheme = function(isDarkMode) {
-    document.body.classList.toggle('theme-dark', isDarkMode);
-    themeColorMeta.setAttribute('content', isDarkMode ? '#111111' : '#c9c9c9');
-
-    if (isDarkMode) {
-      moonIcon.style.opacity = 1;
-      sunIcon.style.opacity = 0;
-    } else {
-      moonIcon.style.opacity = 0;
-      sunIcon.style.opacity = 1;
-    }
-  };
+document.querySelector('.topbar_dark-mode-toggle').addEventListener('click', toggleTheme);
+document.querySelector('.topbar_dark-mode-toggle').addEventListener('keydown', function(event) {
+  if (event.code === 'Space' || event.code === 'Enter') {
+    event.preventDefault();
+    toggleTheme();
+  }
 });
