c5af585cac215327abba0983020e270d7126894e
Silvan Soeters <silvansoeters@gmail.com>
2024-03-30 22:54:32 +0100

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,34 +1,34 @@
-const themeToggles = document.querySelectorAll('.toggle-checkbox');
+const themeToggles = document.querySelectorAll('.topbar\_dark-mode-toggle');
 const themeColorMeta = document.getElementById('theme-color-meta');
 
-themeToggles.forEach(function(themeToggle) {
-  const toggleButton = themeToggle.parentElement;
+themeToggles.forEach(function(toggleButton) {
+  const themeToggle = toggleButton.querySelector('.toggle-checkbox');
   const moonIcon = toggleButton.querySelector('#dark-mode-moon');
   const sunIcon = toggleButton.querySelector('#dark-mode-sun');
 
   toggleButton.addEventListener('click', function() {
-    themeToggle.click();
-    themeToggle.focus();
+    themeToggle.checked = !themeToggle.checked;
+    updateTheme(themeToggle.checked);
   });
 
   toggleButton.addEventListener('keydown', function(event) {
     if (event.code === 'Space' || event.code === 'Enter') {
-      event.preventDefault(); // Prevent default behavior
-      themeToggle.click();
-      themeToggle.focus();
+      event.preventDefault();
+      themeToggle.checked = !themeToggle.checked;
+      updateTheme(themeToggle.checked);
     }
   });
 
-  themeToggle.addEventListener('change', function() {
-    document.body.classList.toggle('theme-dark', this.checked);
-    themeColorMeta.setAttribute('content', this.checked ? '#111111' : '#c9c9c9');
+  const updateTheme = function(isDarkMode) {
+    document.body.classList.toggle('theme-dark', isDarkMode);
+    themeColorMeta.setAttribute('content', isDarkMode ? '#111111' : '#c9c9c9');
 
-    if (this.checked) {
+    if (isDarkMode) {
       moonIcon.style.opacity = 1;
       sunIcon.style.opacity = 0;
     } else {
       moonIcon.style.opacity = 0;
       sunIcon.style.opacity = 1;
     }
-  });
+  };
 });
