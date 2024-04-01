3b5427065b9678fd1b8fce7e4291d3b2309f7f87
Silvan Soeters <silvansoeters@gmail.com>
2024-04-01 22:04:46 +0200

    Update analog-clock.js

--- analog-clock.js
+++ analog-clock.js
@@ -21,3 +21,67 @@
   }
 
   setInterval(displayTime, 1000);
+
+---
+
+<style>
+.clock{
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 50%;
+    background-color: var(--background-color--background-primary);
+    border: 2px solid var(--border-color--border-primary);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.clock span{
+    position: absolute;
+    transform: rotate(calc(30deg * var(--i))); 
+    inset: 12px;
+    text-align: center;
+}
+
+.clock span b{
+    transform: rotate(calc(-30deg * var(--i)));
+    display: inline-block;
+    font-size: 20px;
+}
+
+.clock::before{
+    content: '';
+    position: absolute;
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background-color: var(--background-color--background-alternate);
+    z-index: 2;
+}
+
+.hand{
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+.hand i{
+    position: absolute;
+    background-color: var(--background-color--background-alternate);
+    width: 1.5px;
+    height: var(--h);
+    border-radius: 8px;
+}
+</style>
+
+<div class="clock">
+  <div style="--clr: var(--background-color--background-primary); --h: 0.35rem" id="hour" class="hand">
+    <i></i>
+  </div>
+  <div style="--clr: var(--background-color--background-primary); --h: 0.55rem" id="min" class="hand">
+    <i></i>
+  </div>
+  <div style="--clr: var(--background-color--background-primary); --h: 0.6rem" id="sec" class="hand">
+    <i></i>
+  </div>
+</div>
